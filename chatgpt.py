ddb041e8e62e872ebb36dd0c469b98d1a2f11930
Dana Engebretson <dana.engebretson@gmail.com>
2023-03-17 04:59:26 -0700

    code to get 100 cat haikus from chatgpt

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,6 @@
 import os
 import json
+from pathlib import Path
 
 import requests
 
@@ -12,25 +13,45 @@ openai.Model.list()
 
 URL = "https://api.openai.com/v1/chat/completions"
 
-payload = {
-"model": "gpt-3.5-turbo",
-"messages": [{"role": "user", "content": f"Write 10 haikus about cats"}],
-"temperature" : 1.0,
-"top_p":1.0,
-"n" : 1,
-"stream": False,
-"presence_penalty":0,
-"frequency_penalty":0,
-}
 
-headers = {
-"Content-Type": "application/json",
-"Authorization": f"Bearer {openai.api_key}"
-}
+def ask_gpt(prompt, n_calls, save_path_base_filename):
 
-response = requests.post(URL, headers=headers, json=payload, stream=False)
+    payload = {
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "user", "content": prompt}],
+    "temperature" : 1.0,
+    "top_p":1.0,
+    "n" : 1,
+    "stream": False,
+    "presence_penalty":0,
+    "frequency_penalty":0,
+    }
 
-print(response.content)
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {openai.api_key}"
+    }
 
-with open("haikus_10.json", "w+") as f:
-    json.dump(response.json(), f)
+    for n in range(n_calls):
+
+        save_path_filename = f"{save_path_base_filename}_{n}.json"
+
+        response = requests.post(URL, headers=headers, json=payload, stream=False)
+
+        print(response.content)
+
+        with open(save_path_filename, "w+") as f:
+            json.dump(response.json(), f)
+
+if __name__ == "__main__":
+
+    prompt = "Write 10 haikus about cats"
+    n_calls = 10
+    base_filename = "cat_haikus_10"
+    save_path = "./data/gpt_generated/cat_haikus"
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+
+    save_path_base_filename = f"{save_path}/{base_filename}"
+    print(save_path_base_filename)
+
+    ask_gpt(prompt, n_calls, save_path_base_filename)
